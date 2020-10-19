53722f03ddb906acc35ce95bc18cfb003e48afe0
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-10-19 12:03:33 +0700

    update main.go(get data db from config)

--- main.go
+++ main.go
@@ -11,33 +11,43 @@ import (
 )
  
 func main() {
-        // inisialisasi database dengan memanggil influxConnect()
-        // db := InfluxConnect()
-        // defer db.Close()
-
-        
-        // kita akan melakukan looping secara terus-menerus
-        for {
-                // panggil fungsi getLoad() yang saat ini berada dalam
-                // package collector untuk mendapatkan informasi load yang
-                // telah diparsing, dan lakukan pengecekan, jika terdapat error
-                // kita tampilkan error dan hentikan program
-                loads, err := collector.GetLoad()
-                if err != nil {
-                    log.Fatal(err)
-                }
- 
-                // membuat line protocol dengan menyertakan data loads
-                datapoint := fmt.Sprintf("loadavg,hostname=server-1 load1=%.2f,load5=%.2f,load15=%.2f", loads["load1"], loads["load5"], loads["load15"])
-                // hit api untuk memasukan data loads kedalam database gomondb 
-                data, err := command.ExecCurlInsert("-i", "http://localhost:8086/write?db={your_db_influx}&u={your_username}&p={your_password}", "--data-binary", datapoint)
-                if(err == nil){
-                    log.Println(data)
-                }else{
-                    log.Fatal(err)
-                    break
-                }
-                // beri selang waktu 15 detik sebelum melakukan hal yang sama
-                time.Sleep(15 * time.Second)
+    configvar, err := GetConfig("./config.yml")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // kita akan melakukan looping secara terus-menerus
+    for {
+        // panggil fungsi getLoad() yang saat ini berada dalam
+        // package collector untuk mendapatkan informasi load yang
+        // telah diparsing, dan lakukan pengecekan, jika terdapat error
+        // kita tampilkan error dan hentikan program
+        loads, err := collector.GetLoad()
+        if err != nil {
+            log.Fatal(err)
         }
+ 
+        // membuat line protocol dengan menyertakan data loads
+        datapoint := fmt.Sprintf("loadavg,hostname=server-1 load1=%.2f,load5=%.2f,load15=%.2f", loads["load1"], loads["load5"], loads["load15"])
+        // hit api untuk memasukan data loads kedalam database gomondb 
+        data, err := command.ExecCurlInsert("-i", UriConnection(configvar, "write"), datapoint)
+        if(err == nil){
+            log.Println(data)
+        }else{
+            log.Fatal(err)
+            break
+        }
+        // beri selang waktu 15 detik sebelum melakukan hal yang sama
+        time.Sleep(15 * time.Second)
+    }
+}
+
+func UriConnection(configvar *Config, paramQuery string) (uri string){
+    conf := configvar.Database.Influxdb
+    baseUrl := fmt.Sprintf("http://%s:%s/", conf.Host, conf.Port)
+    db := fmt.Sprintf("%s", conf.Database)
+    username := fmt.Sprintf("%s", conf.Username)
+    password := fmt.Sprintf("%s", conf.Password)
+
+    uri = baseUrl+paramQuery+"?db="+db+"&u="+username+"&p="+password
+    return uri
 }
