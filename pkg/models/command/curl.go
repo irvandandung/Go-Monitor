713b2ae021ba5ddcefe61d3a72d02dab46794913
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-10-19 11:59:39 +0700

    update curl in model

--- pkg/models/command/curl.go
+++ pkg/models/command/curl.go
@@ -4,14 +4,14 @@ import (
     "os/exec"
 )
 
-func ExecCurlInsert(optioncommand string, uri string, optiondata string, data string) (string, error){
-	//declare command curl
-	err := exec.Command("curl", optioncommand, uri, optiondata, data).Run()
-    if err != nil {
-       return "gagal exec", err
-    }
- 	
- 	//return data string & error nil jika command curl berhasil di running
- 	return "success input : "+data, nil
+func ExecCurlInsert(optioncommand string, uri string, data string) (string, error){
+		//declare command curl
+	err := exec.Command("curl", optioncommand, uri, "--data-binary", data).Run()
+	if err != nil {
+	    return "gagal exec", err
+	}
+	 	
+	//return data string & error nil jika command curl berhasil di running
+	return "success input : "+data, nil
 }
 
