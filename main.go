98f3ba1a68b0bd71c9f9ad9058616e978e165d14
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-10-23 10:19:38 +0700

    add data memory & disk to influx in main.go

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
  
 import (
-        "fmt"
-        "log"
-        "time"
-        // import package collector sesuai dengan module path
-        "github.com/irvandandung/gomon/collector"
-        // import package command sesuai dengan module path
-        "github.com/irvandandung/gomon/pkg/models/command"
+    "encoding/json"
+    "fmt"
+    "log"
+    "time"
+    // import package collector sesuai dengan module path
+    "github.com/irvandandung/gomon/collector"
+    // import package command sesuai dengan module path
+    "github.com/irvandandung/gomon/pkg/models/command"
 )
  
 func main() {
@@ -36,6 +37,62 @@ func main() {
             log.Fatal(err)
             break
         }
+
+
+        // panggil collector.GetStorage() yang akan mengembalikan 2 nilai
+        // map[string]string dan error, map[string]string yang berisi
+        // mountpoint dengan informasi mountpoint tersebut akan kita simpan
+        // di variable storages
+        storages, err := collector.GetStorage()
+        if err != nil {
+            log.Fatal(err)
+        }     
+
+        for storage := range storages {
+            // buat s sebagai alias StorageInfo yang berada pada package
+            // collector
+            var s collector.StorageInfo
+            // lakukan unmarshal pada value map, dan simpan hasilnya pada
+            // StorageInfo, yang sebelumnya kita beri alias s
+            if err := json.Unmarshal([]byte(storages[storage]), &s); err != nil {
+                log.Fatal(err)
+            }
+           // buat variable datapoint yang berisi nama mountpoint dan informasi
+            // mengenai total ukuran storage, kapasitas yang tersedia, dan
+            // kapasitas yang sudah digunakan
+            datapoint := fmt.Sprintf("diskusage,hostname=server-1,mountpoint=%s size=%d,avail=%d,used=%d", storage, s.Size, s.Avail, s.Used)
+            // hit api untuk memasukan data diskusage kedalam database gomondb 
+            data, err = command.ExecCurlInsert("-i", UriConnection(configvar, "write"), datapoint)
+            if(err == nil){
+                log.Println(data)
+            }else{
+                log.Fatal(err)
+                break
+            }
+        }  
+
+
+        // panggil fungsi GetMemory() yang berada pada package collector
+        // fungsi tsb akan mengembalikan 2 nilai, map[string]uint64 dan error
+        // map[string]uint64 yang berisi informasi mengenai memori akan
+        // disimpan pada variable memory
+        memory, err := collector.GetMemory()
+        if err != nil {
+            log.Fatal(err)
+        }
+ 
+        // buat line protocol yang berisi informasi mengenai memori
+        memdp := fmt.Sprintf("memory,hostname=server-1 memtotal=%d,memused=%d", memory["MemTotal"], memory["Used"])
+        // hit api untuk memasukan data memory kedalam database gomondb 
+        data, err = command.ExecCurlInsert("-i", UriConnection(configvar, "write"), memdp)
+        if(err == nil){
+            log.Println(data)
+        }else{
+            log.Fatal(err)
+            break
+        }
+
+
         // beri selang waktu 15 detik sebelum melakukan hal yang sama
         time.Sleep(15 * time.Second)
     }
