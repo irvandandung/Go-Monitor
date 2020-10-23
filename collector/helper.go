24fcb6ecb5749b52067bbb8b78bd624ad76ba30f
Muhamad Irvan Dandung <muhamad.dandung@somatech.co>
2020-10-23 10:18:51 +0700

    add function get data from /proc/meminfo

--- collector/helper.go
+++ collector/helper.go
@@ -7,3 +7,8 @@ func toFloat(s string) float64 {
         f, _ := strconv.ParseFloat(s, 64)
         return f
 }
+
+func toUint64(s string) uint64 {
+        u, _ := strconv.ParseUint(s, 10, 64)
+        return u
+}
